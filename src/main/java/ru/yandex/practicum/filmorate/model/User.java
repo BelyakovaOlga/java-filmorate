fe602f191090f59c5c3c50b8b9acd21190cdb198
olecheBel <ovbelyakova11@yandex.ru>
2024-07-14 10:16:47 +0300

    Финальное задание по Sprint10 commit3 после review , ошибки check-repo

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,12 +4,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
-
 import java.time.LocalDate;
 
 @Data
 @Builder
-@FieldDefaults(level= AccessLevel.PRIVATE)
+@FieldDefaults(level=AccessLevel.PRIVATE)
 public class User {
     Long id;
     String email;
