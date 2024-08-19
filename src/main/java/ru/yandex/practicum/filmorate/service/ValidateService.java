e0f22566d6a829f53244ef30b9ca48b1ffda754f
olecheBel <ovbelyakova11@yandex.ru>
2024-08-19 12:00:07 +0300

    Финальное задание по Sprint 12 (порверки repo13)

--- src/main/java/ru/yandex/practicum/filmorate/service/ValidateService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ValidateService.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 @Service
 public class ValidateService {
     private final LocalDate birthdayOfCinema = LocalDate.of(1895, 12, 28);
+
     public void validateUser(User user) {
         if (user.getName() == null || user.getName().isBlank()) {
             throw new ValidationException("Имя должен быть указано");
@@ -28,15 +29,16 @@ public class ValidateService {
             throw new ValidationException("В логине присутсвуют неразрешенные символы (пробелы)");
         }
     }
+
     public void validateFilm(Film film) {
         if (film.getName() == null || film.getName().isBlank()) {
             throw new ValidationException("Название не может быть пустым");
         }
         if (film.getDescription().length() > 200) {
-           throw new ValidationException("Максимальная длина описания превышает 200 символов");
+            throw new ValidationException("Максимальная длина описания превышает 200 символов");
         }
         if (film.getReleaseDate().isBefore(birthdayOfCinema)) {
-           throw new ValidationException("Некорректная дата релиза, дата релиза не может быть меньше даты 28.12.1895");
+            throw new ValidationException("Некорректная дата релиза, дата релиза не может быть меньше даты 28.12.1895");
         }
         if (film.getDuration() < 0) {
             throw new ValidationException("Продолжительность фильма должна быть положительным числом");
